12b0f2eb074eae7140991e5eaf09515ea49b73ab
John Squier <johnsquier@zipcoders-MacBook-Pro-3.local>
2017-02-13 14:57:30 -0500

    created Clock and ClockTest classes to store hours, mins, meridiem

--- src/main/java/squier/john/clockToText/ClockStringParser.java
+++ src/main/java/squier/john/clockToText/ClockStringParser.java
@@ -4,4 +4,10 @@ package squier.john.clockToText;
  * Created by johnsquier on 2/13/17.
  */
 public class ClockStringParser {
+
+    String inputToParse;
+
+    public ClockStringParser(String inputToParse) {
+        this.inputToParse = inputToParse;
+    }
 }
--- src/test/java/squier/john/clockToText/ClockStringParserTest.java
+++ src/test/java/squier/john/clockToText/ClockStringParserTest.java
@@ -1,7 +1,24 @@
 package squier.john.clockToText;
 
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by johnsquier on 2/13/17.
  */
 public class ClockStringParserTest {
+
+    ClockStringParser clockStringParser;
+    String inputToParse;
+
+    @Before
+    public void setup() {
+        inputToParse = "3:30pm";
+        clockStringParser = new ClockStringParser(inputToParse);
+    }
+
+    @Test
+    public void splitClockStringIntoHoursMinsMeridiem() {
+
+    }
 }
